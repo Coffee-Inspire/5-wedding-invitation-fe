8bf3880628ba00b6388c3a1c9757c2f32ec82a5e
Suryanto Chou <suryanto.c@btmbrnch.weefer.co.id>
2022-07-11 01:19:28 +0700

    add transaction info panel

--- src/screens/Gift/index.js
+++ src/screens/Gift/index.js
@@ -1,34 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Row, Col} from 'react-bootstrap'
 import './index.scss'
 
 import Button from '../../components/Button'
+import TransactionPanel from './TransactionPanel'
 
 function Gift() {
 
     const screenContent = {
-        title: "LOVE GIFT",
-        backgroundImage: "https://images.unsplash.com/photo-1610599905570-0dc5693fe584?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        title: "LOVE GIFT",        
         text: "Tanpa mengurangi rasa hormat, bagi anda yang ingin memberikan tanda kasih untuk mempelai, dapat melalui virtual account / E-wallet",
         buttonText: "click here"
     }
 
+    const [showPanel, setShowPanel] = useState(false);
+    const handleShowPanel = () => setShowPanel(true);
+    const handleClosePanel = () => setShowPanel(false);
+
     return (
         <div className='d-flex flex-column justify-content-center cst-gift-wrap'>
-            <img src={screenContent.backgroundImage} className='cst-gift-bg-photo' alt=""/>
             <Row className='m-0 d-flex justify-content-center text-center cst-gift-content'>
-                <Col xs={10} md={5}>
-                    <h3 className='cst-live-title'>
-                        {screenContent.title}
-                    </h3>
-                    <p>
-                        {screenContent.text}
-                    </p>
-                    <Button
-                        className="mt-5"
-                    >
-                        {screenContent.buttonText}
-                    </Button>
+                <Col xs={10} md={6}>
+                    {showPanel ? <TransactionPanel/>                
+                        : <>
+                            <h3 className='cst-live-title'>
+                                {screenContent.title}
+                            </h3>
+                            <p>
+                                {screenContent.text}
+                            </p>
+                            <Button
+                            onClick={()=>handleShowPanel()}
+                            className="mt-5"
+                            >
+                                {screenContent.buttonText}
+                            </Button>
+                        </>                                                     
+                    }
                 </Col>
             </Row>
         </div>
