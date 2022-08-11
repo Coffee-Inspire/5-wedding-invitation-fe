dcd3de43b1de90e76dfa68fa126797892e1ce494
Suryanto Chou <suryanto.c@btmbrnch.weefer.co.id>
2022-08-11 18:44:43 +0700

    adjust responsive image in cover and introduction

--- src/screens/Introduction/index.js
+++ src/screens/Introduction/index.js
@@ -9,14 +9,14 @@ function Introduction() {
   return (
     <>
         <Row className="mx-0 mb-3 cst-intro-row">
-            <Col md={5} className='px-0 d-flex align-items-center'>
+            <Col md={12} lg={5} className='px-0 d-flex justify-content-center align-items-center'>
                 <img
                     src={PhotoGroom}
                     className='cst-intro-photo'
                     alt=""
                 />                
             </Col>
-            <Col md={7} className='d-flex justify-content-center align-items-center'>
+            <Col md={12} lg={7} className='d-flex justify-content-center align-items-center'>
                 <div className='cst-intro-name-card'>
                     <h1 className='py-3 cst-text-highlight-two cst-intro-text-spacing fw-normal'>PETER JIMIE</h1>
                     <p className='m-0'>Putra Kedua Dari :</p>
@@ -25,19 +25,19 @@ function Introduction() {
             </Col>
         </Row>
 
-        <div className='text-center d-block d-md-none'>
+        <div className='cst-intro-and'>
             <h1>&</h1>
         </div>
 
-        <Row className="mx-0 mb-3 cst-intro-row">
-            <Col md={7} className='d-flex justify-content-center align-items-center'>
+        <Row className="mx-0 mb-md-3 cst-intro-row cst-intro-padding-bottom">
+            <Col md={12} lg={7} className='d-flex justify-content-center align-items-center'>
                 <div className='cst-intro-name-card'>
                     <h1 className='py-3 cst-text-highlight-two cst-intro-text-spacing fw-normal'>MEGA PISTIA</h1>
                     <p className='m-0'>Putri Pertama Dari :</p>
                     <p className='m-0'>Lorem ipsum dolor sit amet, consectus adispicing elit. Aliquas quis sodales metus</p>                
                 </div>
             </Col>
-            <Col md={5} className='px-0 d-flex justify-content-end align-items-center'>
+            <Col md={12} lg={5} className='px-0 d-flex justify-content-center align-items-center'>
                 <img
                     src={PhotoBride}
                     className='cst-intro-photo'
