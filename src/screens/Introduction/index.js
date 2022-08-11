573d981e0035579d91a6d823aef812cf094b8ff5
Suryanto Chou <suryanto.c@btmbrnch.weefer.co.id>
2022-08-11 20:31:48 +0700

    adjust responsive image endcover

--- src/screens/Introduction/index.js
+++ src/screens/Introduction/index.js
@@ -17,7 +17,7 @@ function Introduction(props) {
             <Col md={12} lg={5} className='px-0 d-flex align-items-center'>
                 <img
                     src={PhotoGroom}
-                    className='cst-intro-photo animate__animated animate__bounce'
+                    className='cst-intro-photo-one animate__animated animate__bounce'
                     alt=""
                 />                
             </Col>
@@ -36,7 +36,7 @@ function Introduction(props) {
 
         <Row className="mx-0 mb-3 cst-intro-row">
             <Col md={12} lg={7} className='d-flex justify-content-center align-items-center'>
-                <AnimationOnScroll animateIn="animate__fadeIn" animateOnce>
+                <AnimationOnScroll animateIn="animate__fadeIn" animateOnce className='d-flex justify-content-center align-items-center'>
                 <div className='cst-intro-name-card'>
                     <h1 className='py-3 cst-text-highlight-two cst-intro-text-spacing fw-normal'>MEGA PISTIA</h1>
                     <p className='m-0'>Putri Pertama Dari :</p>
@@ -45,11 +45,11 @@ function Introduction(props) {
                 </AnimationOnScroll>
             </Col>
 
-            <Col md={12} lg={5} className='px-0 d-flex justify-content-end align-items-center'>
-                <AnimationOnScroll animateIn="animate__fadeIn" animateOnce>
+            <Col md={12} lg={5} className='p-0'>
+                <AnimationOnScroll animateIn="animate__fadeIn" animateOnce className='px-0 d-flex justify-content-end bg-info align-items-center'>
                 <img
                     src={PhotoBride}
-                    className='cst-intro-photo'
+                    className='cst-intro-photo-two'
                     alt=""
                 />     
                 </AnimationOnScroll>           
